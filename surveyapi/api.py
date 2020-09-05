048c7408373b4d88c8e79ad7475d50a8fee0a82e
Loquen Jones <lrjones416@gmail.com>
2020-09-05 08:03:06 -0700

    create surveys POST route

--- surveyapi/api.py
+++ surveyapi/api.py
@@ -9,12 +9,26 @@ from .models import db, Survey, Question, Choice
 
 api = Blueprint('api', __name__)
 
-@api.route('/surveys/')
+@api.route('/surveys/', methods=('GET', 'POST'))
 def surveys():
-  surveys = Survey.query.all()
-  return { 'surveys': [s.to_dict() for s in surveys] }
+  if request.method == 'GET':
+    surveys = Survey.query.all()
+    return { 'surveys': [s.to_dict() for s in surveys] }
+  elif request.method == 'POST':
+    data = request.get_json() # translate incoming JSON to a python dict
+    survey = Survey(name=data['name'])
+    questions = []
+    for q in data['questions']:
+      question = Question(text=q['text'])
+      question.choices = [Choice(text=c['text']) for c in q['choices']]
+      questions.append(question)
+    survey.questions = questions
+    db.session.add(survey)
+    db.session.commit()
+    return survey.to_dict(), 201
 
 @api.route('/surveys/<int:id>/')
 def survey(id):
   survey = Survey.query.get(id)
   return { 'survey': survey.to_dict() }
+
